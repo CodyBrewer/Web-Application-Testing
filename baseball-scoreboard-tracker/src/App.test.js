5d11f6b362aa8f9b4d2b2e1e22049881755becb3
CodyBrewer <brewer.cody.m@gmail.com>
2020-01-23 18:05:27 -0700

    app tests passing

--- baseball-scoreboard-tracker/src/App.test.js
+++ baseball-scoreboard-tracker/src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
+import Display from "./components/Display";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(cleanup);
+
+describe("App", () => {
+  test("should render without crashing", () => {
+    render(<App />);
+  });
+  test("should render Display component without crashing", () => {
+    render(<Display />);
+  });
 });
